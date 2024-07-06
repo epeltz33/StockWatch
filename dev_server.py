1efcff6989540d2e75e2ef60df8e12558528c728
Eric Peltzman <epeltz@optonline.net>
2024-07-06 17:58:51 +0000

    got codespace env setup

--- dev_server.py
+++ dev_server.py
@@ -1,6 +1,10 @@
-from app import create_app
+from app import create_app, db
 
 app = create_app()
+
+# Ensure the database is created
+with app.app_context():
+    db.create_all()
 
 if __name__ == '__main__':
     app.run(debug=True)
--- test_env.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-# Load environment variables from .env file
-load_dotenv()
-
-print(os.getenv('POLYGON_API_KEY'))
-print(os.getenv('APP_NAME'))
-
